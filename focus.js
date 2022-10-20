aa00b56cc98c84448b813a2ab611b41d83fa76c9
unknown <ravi943117@gmail.com>
2022-10-21 03:11:20 +0530

    table data added by object

--- focus.js
+++ focus.js
@@ -1,4 +1,42 @@
+const tableData = document.getElementById("usertable");
 
+let usersData =[
+    {users:'jonas', data:'1234', deviceId:'34576@8589', imeiNo:'https://news.google.com',id:1},
+    {users:'jokutyr', data:'2453', deviceId:'57666@8584', imeiNo:'https://www.youtube.com',id:2},
+    {users:'gtrewse', data:'5674', deviceId:'85656@8566', imeiNo:87675634235775,id:3},
+    {users:'mosaid', data:'7454', deviceId:'34576@8586', imeiNo:76765567567565,id:4},
+    {users:'jonas', data:'1234', deviceId:'34576@8589', imeiNo:84536974389743,id:5},
+    {users:'jokutyr', data:'2453', deviceId:'57666@8584', imeiNo:86554464453424,id:6},
+    {users:'gtrewse', data:'5674', deviceId:'85656@8566', imeiNo:87675634235775,id:7},
+    {users:'mosaid', data:'7454', deviceId:'34576@8586', imeiNo:76765567567565,id:8},
+    {users:'jonas', data:'1234', deviceId:'34576@8589', imeiNo:84536974389743,id:9},
+    {users:'jokutyr', data:'2453', deviceId:'57666@8584', imeiNo:86554464453424,id:10},
+    {users:'gtrewse', data:'5674', deviceId:'85656@8566', imeiNo:87675634235775,id:11},
+    {users:'mosaid', data:'7454', deviceId:'34576@8586', imeiNo:76765567567565,id:12},
+    {users:'jonas', data:'1234', deviceId:'34576@8589', imeiNo:84536974389743,id:13},
+    {users:'jokutyr', data:'2453', deviceId:'57666@8584', imeiNo:86554464453424,id:14},
+    {users:'gtrewse', data:'5674', deviceId:'85656@8566', imeiNo:87675634235775,id:15},
+    {users:'mosaid', data:'7454', deviceId:'34576@8586', imeiNo:76765567567565,id:16},
+    {users:'jonas', data:'1234', deviceId:'34576@8589', imeiNo:84536974389743,id:17},
+    {users:'jokutyr', data:'2453', deviceId:'57666@8584', imeiNo:86554464453424,id:18},
+    {users:'gtrewse', data:'5674', deviceId:'85656@8566', imeiNo:87675634235775,id:19},
+    {users:'mosaid', data:'7454', deviceId:'34576@8586', imeiNo:76765567567565,id:20},
+]
+
+function userlist(data) {
+    let html='';
+    data.forEach(user => {
+       
+        html +=` <tr>
+        <td><input type="checkbox">${user.users}</td>
+        <td>${user.data}</td>
+        <td>${user.deviceId}</td>
+        <td><a target="_blank" href=${user.imeiNo}>${user.imeiNo}</a></td>
+    </tr>`
+    });
+    tableData.insertAdjacentHTML("beforeend", html)
+  }
+  userlist(usersData)
 $(document).ready(function(){
 $("#item-btn").click(function(){
     console.log("click")
